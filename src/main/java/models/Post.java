9bd628d20e7bd83c90f554692e2ea3c16f917962
Perry Eising <perry.r.eising@gmail.com>
2017-07-10 13:25:12 -0700

    initial commit with Post class and two tests

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Created by staff on 7/10/17.
+ */
+public class Post {
+
+    private final String content;
+
+    public Post (String content){
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,33 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by staff on 7/10/17.
+ */
+public class PostTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+
+    }
+}
